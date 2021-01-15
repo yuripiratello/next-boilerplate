5eabafdd820de817b07447255e884aaf45007c45
Yuri Piratello <yuri.piratello@gmail.coml>
2021-01-15 16:45:19 -0300

    a lot of stuffs

---
+++ src/pages/_app.tsx
@@ -0,0 +1,22 @@
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta
+          name="description"
+          content="A simple project startr to work with TypeScript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
